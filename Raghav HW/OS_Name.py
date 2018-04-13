01e9f51074e0fb22f3143a205830a6bc0c4f54b6
kapil <shukla.kapil@gmail.com>
2018-04-13 23:38:48 +1000

    fixing syntax error

--- Raghav HW/OS_Name.py
+++ Raghav HW/OS_Name.py
@@ -2,4 +2,4 @@ import platform
 import os
 print("The Operating System is: " + os.name)
 print("The platform is: " + platform.system())
-print("The platform release was: " platform.release())
+print("The platform release was: " platform.release()
