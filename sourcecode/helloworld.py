007a030578701a45d5b9fd5bcdeb3317a501df34
kapil <shukla.kapil@gmail.com>
2018-07-29 22:33:55 +1000

    clean
    cleanup

--- sourcecode/helloworld.py
+++ sourcecode/helloworld.py
@@ -1,19 +1,3 @@
-"""
-This module lets its users gets familiar with basic syntax of print function and gets a user started with conventional
-'Hello World' program.
-"""
-
-
-name = input("Plese enter your name")
-print("Hello %s"  % name)
-a = 6
-b = 10
 print("today is sat")
-
-def testfunc():
-    print("test func")
-
-
-
 
 
