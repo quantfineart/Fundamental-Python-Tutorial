eb027457fefef49185226e928299e9a16c69a543
kapil <shukla.kapil@gmail.com>
2018-07-29 22:37:40 +1000

    Correct Hello world msg

--- sourcecode/helloworld.py
+++ sourcecode/helloworld.py
@@ -1,3 +1,9 @@
-print("today is sat")
+"""
+Date            :   29Jul2018
+Description     :   Introductory module for welcome and demonstrate the print function.
+Author          :   Kapil Shukla
 
+"""
+
+print("Hello to the Wonderful World of Python!!")
 
